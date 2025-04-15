9c9b95de127866d71b3eee4c6295af9bd5e55115
Albertohuanuco <albertohuanuco01@gmail.com>
2025-04-14 23:16:23 -0500

    condicionales

--- funciones.py
+++ funciones.py
@@ -1,10 +1,52 @@
-
+#Cuenta la cantidad de letras
 y=len("Carlos")
 print(y)
 
-w=len("Carlos")
+w=len("Carlota")
 print(w)
 
 h=ascii("hola")
 print(h)
 
+c=chr(65)
+print(c)
+
+#Devuelve el valor absoluto de un número
+d=abs(-45)
+print(d)
+
+#Convrtir número decimal a binario
+e=bin(234)
+print(e)
+
+#Da el máximo valor en un rango de números
+r=max(2,2,3,4,5,9)
+print(r)
+ 
+#Arroja el minimo valor en un rango de números
+t=min(7,6,8,4,3,2,1)
+print(t)
+
+#Para sumar una lista de números
+u=sum([4,5,6,7,8])
+print(u) 
+
+#Probando condicionales "if"
+edad=20
+if edad>=15 and edad <=30:
+    print("ok")
+    
+name= "Jherson"
+if name=="Jherson":
+    print("OK")    
+else: 
+    print("Incorrecto")
+    
+age=19
+if age<18:
+     print("Kid")
+else:
+    print("Adult")
+         
+
+#Probando "Switch"
