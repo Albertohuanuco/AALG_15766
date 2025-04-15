9da25c3d2ae7fc6c56187143aa4a961f8303cfbe
Albertohuanuco <albertohuanuco01@gmail.com>
2025-04-14 21:47:37 -0500

    operaciones práctica

---
+++ program2.py
@@ -0,0 +1,22 @@
+#Exponente
+a=2**3
+print(a)
+print("El valor de a es: ", a)
+#multiplicación
+b=2*3
+print(b)
+#División
+c=8/2
+print(c)
+#División entera
+d=8//3
+print(d)
+#Módulo/resto
+e=22%8
+print(e)
+#Suma
+f=4+8
+print(f)
+#Resta
+j=7-4
+print(j)
