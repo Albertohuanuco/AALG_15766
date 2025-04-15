4b94b15c70aba69a86d6e5d2f8187bf68eab4a28
Albertohuanuco <albertohuanuco01@gmail.com>
2025-04-14 22:35:57 -0500

    archivos imprimir e ingreso

---
+++ ingreso_datos.py
@@ -0,0 +1,8 @@
+print("What's your name?")
+n= input()
+print(f"Hello {n}")
+
+x=input("What's your name?\n")
+print(f"Hello {x}")
+
+
---
+++ program3.py
@@ -0,0 +1,13 @@
+a="Carlos"
+b=30
+print("Hello", a,"you are", b)
+
+c="Hello "+ str(a)+ " you are "+ str(b)
+print(c)
+
+d=f"Hello {a} you are {b}"
+print(d)
+
+print("Carlos", "Jose", "Juan")
+print("Carlos", "Jose", "Juan", sep="* ", end="%\n")
+print("Carlos", "Jose", "Juan")
